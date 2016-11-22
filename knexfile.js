e76a9e16fef7b66b69c24cf61434e06736ac41b6
DerekWW <derek.wierson@gmail.com>
2016-11-22 14:28:58 -0800

    add knex files

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/movie_db'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
