443a847bcd2da850c0c348232b0fbea43f6821f4
DerekWW <derek.wierson@gmail.com>
2016-12-05 09:28:09 -0800

    fix CSRF bug

--- server.js
+++ server.js
@@ -23,13 +23,12 @@ app.use(cookieParser());
 app.use(express.static(path.join('public')));
 
 // CSRF protection
-app.use((req, res, next) => {
-  if (/json/.test(req.get('Accept'))) {
-    return next();
-  }
-
-  res.sendStatus(406);
-});
+// const app.use((req, res, next) => {
+//   if (/json/.test(req.get('Accept'))) {
+//     return next();
+//   }
+//   res.sendStatus(406);
+// });
 
 const users = require('./routes/users');
 const friends = require('./routes/friends');
